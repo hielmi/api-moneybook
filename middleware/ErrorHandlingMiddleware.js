286cccc4c8096751ee7663b1080f1f0ce094318b
Hielmi Sulaeman <hielmisulaeman10@gmail.com>
2024-03-19 11:47:20 +0700

    fix: fix errorHandlingMiddleware response

--- middleware/ErrorHandlingMiddleware.js
+++ middleware/ErrorHandlingMiddleware.js
@@ -1,11 +1,10 @@
-const errorHandlingMiddlware = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 404;
-  err.customMessage = err.customMessage || "Something Error";
-  return res.status(err.statusCode).json({
-    status: err.statusCode,
-    message:
-      process.env.NODE_ENV === "production" ? err.customMessage : err.message,
+const errorHandlingMiddleware = (err, req, res, next) => {
+  const errorMessage =
+    err.customMessage.customMessage || "Something went wrong";
+  return res.status(err.statusCode || 500).json({
+    status: err.statusCode || 500,
+    message: process.env.NODE_ENV === "production" ? errorMessage : err.message,
   });
 };
 
-module.exports = errorHandlingMiddlware;
+module.exports = errorHandlingMiddleware;
