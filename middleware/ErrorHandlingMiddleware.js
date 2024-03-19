05c4a1417f71b7e22c1fbc00e61b0f667b59b393
Hielmi Sulaeman <hielmisulaeman10@gmail.com>
2024-03-19 12:50:23 +0700

    fix: bug handling Error middleware:

--- middleware/ErrorHandlingMiddleware.js
+++ middleware/ErrorHandlingMiddleware.js
@@ -1,6 +1,7 @@
 const errorHandlingMiddleware = (err, req, res, next) => {
+  console.log(err);
   const errorMessage =
-    err.customMessage.customMessage || "Something went wrong";
+    err?.customMessage?.customMessage || "Something went wrong";
   return res.status(err.statusCode || 500).json({
     status: err.statusCode || 500,
     message: process.env.NODE_ENV === "production" ? errorMessage : err.message,
